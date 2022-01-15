121c1a1814a636f3b5ea7fafcc38992610514fb2
Peter Kingsley <peterjkingsley@hotmail.com>
2022-01-15 11:44:10 -0800

    Updated file structure, copied auth.js from week 22 activity 26

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+//File taken from week 22 activity 26
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
