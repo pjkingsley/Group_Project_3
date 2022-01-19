9ba2b033710aa2fd6948317a1b995a617f88ce14
Sara K <sarasuni2021@gmail.com>
2022-01-18 23:16:02 -0800

    Modified App.js. Added line of code to display the login/register form

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/ui/Header'
 import Side_Bar from './components/ui/Side-Bar'
 import Login_Signup from './components/ui/Login-Signup'
+import RendererApp from './components/login/renderer.js';
 const App = ()=>{
   return (
     <div className="body-container">
       <Header/>
       <div id="main-body"> 
           <Side_Bar/>
+          <RendererApp/>
           <Login_Signup/>
       </div>
       </div>
