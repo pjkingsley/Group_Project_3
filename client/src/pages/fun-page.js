0776fda3835827b1802d46835575d5c24ce5483f
MissingNewt <chelby.sallady@gmail.com>
2022-01-28 09:01:13 -0800

    adding fun to the fun page, adding some media queries, will add more later

--- client/src/pages/fun-page.js
+++ client/src/pages/fun-page.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../App.css';
+import './css/funpage.css'
+import axios from 'axios';
 
 const FunPage = () => {
-  return <div>
-      {/* Im figuring out a way to do target click random generate... */}
-      <p>Hello World, don't mind me.</p>
-  </div>;
-};
 
+        const getPun  = async()=>{
+            var pun = await axios.get("https://my-bao-server.herokuapp.com/api/breadpuns");
+            var randomFact = document.getElementById('pun')
+            randomFact.innerHTML = pun.data;
+            console.log(pun.data);
+            
+        }
+            return (<div className="FunPage">
+            <button id= "Pun-Button" onClick={_ => getPun()}>Press me for a surprise!</button>
+            <p id='pun'></p>
+            <p id='secret'>Everyone needs a good bread pun or two in their life.</p>
+        </div>
+      );
+    };
 
 export default FunPage;
