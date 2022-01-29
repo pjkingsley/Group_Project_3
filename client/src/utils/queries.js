5a8d46843b0c3acf9fe52bc0c92b2f5b9ce5319a
Peter Kingsley <peterjkingsley@hotmail.com>
2022-01-29 13:01:34 -0800

    commit after merge

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-<<<<<<< HEAD
 export const QUERY_RECIPES = gql`
   query allRecipes {
       recipes {
@@ -12,49 +11,6 @@ export const QUERY_RECIPES = gql`
         ingredients
         instructions
         likes
-=======
-export const QUERY_USER = gql`
-  query profile($userName: String!) {
-    profile(userName: $userName) {
-      _id
-      userName
-      authored {
-        _id
-        name
-        date
-      }
-    }
-  }
-`;
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_RECIPES = gql`
-  query getRecipes($recipe: ID) {
-    recipes(recipe: $recipe) {
-      _id
-      name
-      image
-      description
-      author
-      ingredients
-      instructions
-      likes
-      date
->>>>>>> ce1c0894babea678a89f33ad543ab1fbc0c9d561
     }
   }
 `;
